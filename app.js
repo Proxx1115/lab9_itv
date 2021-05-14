9fbd4a6c467707edf8d14c3f66fd38052db41c33
Proxx1115 <79000915+Proxx1115@users.noreply.github.com>
2021-05-14 14:22:09 +0800

    Update app.js
    aldaag zassan

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var PASSWORD = "Premium123"
 var currentPrice = 0;
 var totalPrice = 0;
 
+
 isLogin = false;
 
 function check(){
@@ -24,67 +25,23 @@ function check(){
 
 var n;
 function pay(n){
-       if(isLogin === true){
-        if(n === 1){
-            currentPrice = currentPrice + parseInt(document.getElementById("1").textContent);
-            document.getElementById("prc").textContent = currentPrice;
-        }
-        if(n === 2){
-            currentPrice = currentPrice + parseInt(document.getElementById("2").textContent);
-            document.getElementById("prc").textContent = currentPrice;
-        }
-        if(n === 3){
-            currentPrice = currentPrice + parseInt(document.getElementById("3").textContent);
-            document.getElementById("prc").textContent = currentPrice;
-        }
-        if(n === 4){
-            currentPrice = currentPrice + parseInt(document.getElementById("4").textContent);
-            document.getElementById("prc").textContent = currentPrice;
-        }
-        if(n === 5){
-            currentPrice = currentPrice + parseInt(document.getElementById("5").textContent);
-            document.getElementById("prc").textContent = currentPrice;
-        }
-        if(n === 6){
-            currentPrice = currentPrice + parseInt(document.getElementById("6").textContent);
-            document.getElementById("prc").textContent = currentPrice;
-        }
-        if(n === 7){
-            currentPrice = currentPrice + parseInt(document.getElementById("7").textContent);
-            document.getElementById("prc").textContent = currentPrice;
-        }
-        if(n === 8){
-            currentPrice = currentPrice + parseInt(document.getElementById("8").textContent);
-            document.getElementById("prc").textContent = currentPrice;
-        }
-        if(n === 9){
-            currentPrice = currentPrice + parseInt(document.getElementById("9").textContent);
-            document.getElementById("prc").textContent = currentPrice;
-        }
-        if(n === 10){
-            currentPrice = currentPrice + parseInt(document.getElementById("10").textContent);
-            document.getElementById("prc").textContent = currentPrice;
-        }
-        if(n === 11){
-            currentPrice = currentPrice + parseInt(document.getElementById("11").textContent);
-            document.getElementById("prc").textContent = currentPrice;
-        }
-        if(n === 12){
-            currentPrice = currentPrice + parseInt(document.getElementById("12").textContent);
-            document.getElementById("prc").textContent = currentPrice;
-        }
-    }else{
+    if(isLogin === true){
+        currentPrice = currentPrice + parseInt(document.getElementById(n).textContent);
+        document.getElementById("prc").textContent = currentPrice;
+    }
+    else{
         alert("Та premium эрхээрээ Login хийж нэвтэрнүү!!!");
     }
     
 }
 
 function toloh(){
-    alert("Та " + currentPrice + " төгрөгний төлбөрөө амжилттай хийлээ.");
-    currentPrice = 0;
+    alert("Та " + currentPrice + " төгрөгний төлбөрөө амжилттай хийлээ.")
+    currentPrice =0;
     end();
 }
 
 function end(){
     document.getElementById("prc").textContent = 0;
+
 }
